81a4da190e28f8634af1e32032b2407c0bac066f
Bekir Yenidoğan <bekiryenidogan16@gmail.com>
2022-09-21 16:18:41 +0300

    largestNumber

---
+++ largestNumber.rs
@@ -0,0 +1,19 @@
+
+
+fn largest ( list: &[i32]) -> i32 {
+    let mut largest = list[0];
+ 
+    for &item in list {
+       if item > largest {
+          largest = item;
+       }
+    }
+    largest
+ }
+ fn main(){
+    let number_list = vec![12,231,1251,634,143,33];
+ 
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+ }
+ 
